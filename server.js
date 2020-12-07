bd66d955545f6b5ba3c3beb7d07f26083766cba3
paymon123 <paymon123@gmail.com>
2020-12-06 23:37:53 -0500

    b

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 const { Client } = require('pg');
+
 //const game = require("./game")
 
 let DB_URI = process.env.DATABASE_URL;
@@ -22,24 +23,10 @@ app.use(express.urlencoded({
 }))
 
 
-app.use(function(req, res, next) {
-  console.log("404");
-const err = new ExpressError("Not Found", 404);
-// pass the error to the next piece of middleware
-return next(err);
-});
 
 
 /** general error handler */
 
-app.use(function(err, req, res, next) {
-res.status(err.status || 500);
-console.log("error being passed: " +err);
-return res.json({
-  status: err.status,
-  message: err.message
-});
-});
 app.get('/', function (req, res) {
    
   console.log("hello")
